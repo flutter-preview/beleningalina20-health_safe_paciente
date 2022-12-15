805925a169234712754eee1005cd6d09d61bc014
beleningalina20 <ingalinaa.mb@gmail.com>
2022-12-15 10:19:33 -0300

    Inicializacion Health Safe - PACIENTE

---
+++ android/app/src/main/kotlin/com/example/health_safe_paciente/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.health_safe_paciente
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:health_safe_paciente/src/app.dart';
+
+void main() => runApp(const MyApp());
