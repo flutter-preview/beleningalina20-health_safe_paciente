2d3633086ebb2c41aa662e913109c9996bec1741
beleningalina20 <ingalinaa.mb@gmail.com>
2023-01-05 11:12:08 -0300

    Agrega el theme para app bar

--- lib/src/theme/theme_app.dart
+++ lib/src/theme/theme_app.dart
@@ -2,22 +2,22 @@ import 'package:flutter/material.dart';
 
 class ThemeApp {
   static ThemeData theme = ThemeData(
-      textTheme: const TextTheme(
-        // ! Puede ser removido las propiedades de este text theme por que son 2018
-        // Logo - Nombre app
-        headline1: TextStyle(fontSize: 30, fontWeight: FontWeight.w400),
-        // Logo - Slogan
-        headline2: TextStyle(fontSize: 25, fontWeight: FontWeight.w300),
-        // Labels para los text field custom
-        // TextField
-        subtitle1: TextStyle(fontSize: 15),
-        // Text
-        bodyText2: TextStyle(fontSize: 18),
-        // Texto que acompaña al text button
-        button: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+    textTheme: const TextTheme(
+      // ! Puede ser removido las propiedades de este text theme por que son 2018
+      // Logo - Nombre app
+      headline1: TextStyle(fontSize: 30, fontWeight: FontWeight.w400),
+      // Logo - Slogan
+      headline2: TextStyle(fontSize: 25, fontWeight: FontWeight.w300),
+      // Labels para los text field custom
+      // TextField
+      subtitle1: TextStyle(fontSize: 15),
+      // Text
+      bodyText2: TextStyle(fontSize: 18),
+      // Texto que acompaña al text button
+      button: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
 
-        // Sin usar
-        /*subtitle2: TextStyle(color: Colors.green),
+      // Sin usar
+      /*subtitle2: TextStyle(color: Colors.green),
         headline3: TextStyle(color: Colors.purple, fontWeight: FontWeight.w400),
         caption: TextStyle(color: Colors.purple),
 
@@ -27,26 +27,34 @@ class ThemeApp {
         headline6: TextStyle(color: Colors.red, fontWeight: FontWeight.w400),
         bodyText1: TextStyle(color: Colors.red),
         overline: TextStyle(color: Colors.red),*/
-      ),
-      elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-              padding: const EdgeInsets.all(15),
-              textStyle:
-                  const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8.25)))),
-      textButtonTheme: TextButtonThemeData(
-          style: TextButton.styleFrom(
-              foregroundColor: Colors.blue,
-              textStyle:
-                  const TextStyle(fontSize: 18, fontWeight: FontWeight.w400))),
-      dialogTheme: DialogTheme(
-          actionsPadding: const EdgeInsets.all(15),
-          alignment: Alignment.center,
-          contentTextStyle: const TextStyle(color: Colors.black, fontSize: 16),
-          iconColor: Colors.blue,
-          titleTextStyle: const TextStyle(
-              fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(8.25))));
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.all(15),
+            textStyle:
+                const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.25)))),
+    textButtonTheme: TextButtonThemeData(
+        style: TextButton.styleFrom(
+            foregroundColor: Colors.blue,
+            textStyle:
+                const TextStyle(fontSize: 18, fontWeight: FontWeight.w400))),
+    dialogTheme: DialogTheme(
+        actionsPadding: const EdgeInsets.all(15),
+        alignment: Alignment.center,
+        contentTextStyle: const TextStyle(color: Colors.black, fontSize: 16),
+        iconColor: Colors.blue,
+        titleTextStyle: const TextStyle(
+            fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.25))),
+    appBarTheme: AppBarTheme(
+        actionsIconTheme: IconThemeData(color: Colors.grey[600]),
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        elevation: 3.0,
+        iconTheme: IconThemeData(color: Colors.grey[600]),
+        shadowColor: Colors.black),
+  );
 }
